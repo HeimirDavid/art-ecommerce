096ad76e27de1013136dd1b9b4351204319d2fc9
HeimirDavid <heimir.97@gmail.com>
2020-04-23 11:14:17 +0200

    First model created for News/Blog posts in newsposts/models.py

--- newsposts/models.py
+++ newsposts/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+from django.utils import timezone
+
+
+class NewsPost(models.Model):
+    """
+    A News/Blog Post.
+    Same model design as 'Django Blog Miniproject' from Code Institutes Code Along
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    views = models.IntegerField(default=0)
+    tag = models.CharField(max_length=30, blank=True, null=True)
+    image = models.ImageField(upload_to="img", blank=True, null=True)
+
+    def __unicode__(self):
+        return self.title
