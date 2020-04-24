e1b5d486d177d127da00ff5b5707348af053ac5d
HeimirDavid <heimir.97@gmail.com>
2020-04-24 11:11:10 +0200

    Modified Product table to allow 3 different prints for each product

--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ class OriginalPainting(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     size = models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return self.stock
+
 
 class PrintPainting(models.Model):
     size = models.CharField(max_length=100)
@@ -21,7 +24,9 @@ class Product(models.Model):
     description = models.TextField()
     image = image = models.ImageField(upload_to="img", blank=False, null=False)
     original_painting = models.ForeignKey(OriginalPainting, on_delete=models.CASCADE, null=False)
-    prints = models.ForeignKey(PrintPainting, on_delete=models.CASCADE, blank=True,null=True)
+    print_1 = models.ForeignKey(PrintPainting, on_delete=models.CASCADE, related_name="print_one", blank=True,null=True)
+    print_2 = models.ForeignKey(PrintPainting, on_delete=models.CASCADE, related_name="print_two", blank=True,null=True)
+    print_3 = models.ForeignKey(PrintPainting, on_delete=models.CASCADE, related_name="print_three", blank=True,null=True)
     collection = models.ForeignKey(CollectionCategory, on_delete=models.CASCADE)
 
 
