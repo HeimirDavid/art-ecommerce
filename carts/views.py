26e8d5a95c2fc832e82b0c2aa443cb30aa81c914
HeimirDavid <heimir.97@gmail.com>
2020-04-27 18:28:52 +0200

    Added sessions to link a specific user to a specific Cart table

--- carts/views.py
+++ carts/views.py
@@ -6,17 +6,33 @@ from products.models import PrintPainting, Product
 
 
 def view_cart(request):
+    try:
+        cart_session_id = request.session['cart_id']
+    except:
+        cart_session_id = None
+    if cart_session_id:
+        cart = Cart.objects.get(id=cart_session_id)
+        context = {"cart": cart}
+    else:
+        empty_message = "Your cart is empty, please keep shopping."
+        context = {"empty": True, 'empty_message': empty_message}
 
-    cart = Cart.objects.all()[0]
     template = "cart.html"
-    prints = PrintPainting.objects.all()
-    context = {"cart": cart, "prints": prints}
-
     return render(request, template, context)
 
 
 def update_cart(request, pk):
-    cart = Cart.objects.all()[0]
+    request.session.set_expiry(300000)
+    try:
+        cart_session_id = request.session['cart_id']
+    except:
+        new_cart = Cart()
+        new_cart.save()
+        request.session['cart_id'] = new_cart.id
+        cart_session_id = new_cart.id
+
+    cart = Cart.objects.get(id=cart_session_id)
+
     try: 
         product = Product.objects.get(pk=pk) 
     except Product.DoesNotExist:
