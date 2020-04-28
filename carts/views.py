fa13fa2034ed5e2df31bdaaa45639c9ddc33540b
HeimirDavid <heimir.97@gmail.com>
2020-04-28 22:00:49 +0200

    add_to_cart altered to check if the value from the POST request is 'size' or 'original' to be able handle both instances differently. add_to_cart successfully adds a print or original ainting to the cart

--- carts/views.py
+++ carts/views.py
@@ -5,6 +5,7 @@ from .models import Cart, CartItem
 from products.models import PrintPainting, Product
 
 
+
 def view_cart(request):
     try:
         cart_session_id = request.session['cart_id']
@@ -13,12 +14,21 @@ def view_cart(request):
     if cart_session_id:
         cart = Cart.objects.get(id=cart_session_id)
         new_total = 0.00
+
+
+        items = cart.cartitem_set.all()
+        print(items)
+
+
         for item in cart.cartitem_set.all():
-            line_total_for_product = float(item.product.original_painting.price) * item.quantity          
+            print(item.product.id)
+            line_total_for_product = float(item.line_total) * item.quantity          
             new_total += line_total_for_product
         request.session['items_total'] = cart.cartitem_set.count()
         cart.total = new_total
         cart.save()
+
+
         context = {"cart": cart}
     else:
         empty_message = "Your cart is empty, please keep shopping."
@@ -36,7 +46,7 @@ def remove_from_cart(request, pk):
     except:
         return redirect(reverse('view_cart'))
     cartitem = CartItem.objects.get(pk=pk)
-    print(cartitem)
+    #print(cartitem)
     cartitem.cart = None
     cartitem.save()
     # Send message that it's deleted
@@ -46,8 +56,9 @@ def remove_from_cart(request, pk):
 
 def add_to_cart(request, pk):
     request.session.set_expiry(300000)
-
-    # Get the cart
+    # Check/Try if an instance of a cart session already exists and matches,
+    # Otherwise create a new instance, match it with the id of the cart 
+    # so it's unique for the current user.
     try:
         cart_session_id = request.session['cart_id']
     except:
@@ -55,42 +66,59 @@ def add_to_cart(request, pk):
         new_cart.save()
         request.session['cart_id'] = new_cart.id
         cart_session_id = new_cart.id
-
     cart = Cart.objects.get(id=cart_session_id)
+    
 
     # get the product
-    try: 
+    try:
         product = Product.objects.get(pk=pk)
-        product_prints = PrintPainting.objects.get(pk=pk)
-        print(product_prints)
+        #print(product_prints)
     except Product.DoesNotExist:
         pass
-    except:
-        pass
-
 
     
     # Get the items and their quantity from the form from the user
     product_var = []
     if request.method == "POST":
-        qty = request.POST['qty']
+        try:
+            qty = request.POST['qty']
+        except:
+            pass
 
+        try:
+            original = request.POST['original']
+            qty = int(1)
+            #print(original)
+        except:
+            pass
+
+        cart_item = CartItem.objects.create(cart=cart, product=product)
         for item in request.POST:
+            if item == "original":
+                #print("jibbicola")
+                #print(qty)
+                #print(pk)
+                cart_item.product = product
+                cart_item.quantity = qty
+                cart_item.line_total = product.original_painting.price
+                cart_item.save()
+                return redirect(reverse('view_cart'))
+    
             if item == "size":
-                print(item)
                 key = item
                 val = request.POST[key]
-                print(item)
                 try:
-                    printSize = PrintPainting.objects.get(product=product, id=val)
-                    product_var.append(printSize)
+                    single_product = PrintPainting.objects.get(product=product, id=val)
+                    price = single_product.price
+                    product_var.append(single_product)
                 except:
                     pass
 
-        cart_item = CartItem.objects.create(cart=cart, product=product)
+        #cart_item = CartItem.objects.create(cart=cart, product=product)
         
         if len(product_var) > 0:
             cart_item.print_variations.add(*product_var)
+        cart_item.line_total = price
         cart_item.quantity = qty
         cart_item.save()
         # Success message
