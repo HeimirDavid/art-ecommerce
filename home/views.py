146fa8ea1486b7c78c208c2da1544ad3187516d0
HeimirDavid <heimir.97@gmail.com>
2020-04-29 15:27:38 +0200

    Products added to index.html to display some paintings straight away

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from newsposts.models import NewsPost
+from products.models import Product
 
 
 # Create your views here.
@@ -8,4 +9,7 @@ def index(request):
 
     posts = NewsPost.objects.filter(published_date__lte=timezone.now
         ()).order_by('-published_date')
-    return render(request, 'index.html', {'posts': posts})
+
+    products = Product.objects.all()
+    context = {'posts': posts, 'products': products}
+    return render(request, 'index.html', context)
