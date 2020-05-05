fb23cd2dff28e8c6fce6a7747bc62682d01afec0
HeimirDavid <heimir.97@gmail.com>
2020-05-05 11:24:13 +0200

    bootstrap alerts added to contact form to notify the user upon successfully sent email or reverse

--- static/js/sendEmailJS.js
+++ static/js/sendEmailJS.js
@@ -1,7 +1,19 @@
-console.log("HEllo World")
+/*
+var templateParams = {
+    "from_name": contactForm.full_name.value,
+    "from_email": contactForm.email_address.value,
+    "text_field": contactForm.message.value
+}*/
+
+var successAlert = document.getElementById("success_alert");
+
+var failedAlert = document.getElementById("failed_alert");
+
+console.log(successAlert, failedAlert)
+
+
 
 function sendMail(contactForm) {
-    console.log("Hejhopp")
     emailjs.send("gmail", "art_store", {
         "from_name": contactForm.full_name.value,
         "from_email": contactForm.email_address.value,
@@ -10,9 +22,14 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            contactForm.reset()
+            //alert("Your email has been sent!")
+            //console.log(successAlert)
+            successAlert.classList.add("show")
         },
         function(error) {
             console.log("FAILED", error)
+            failedAlert.classList.add("show")
         }
     )
     return false;
