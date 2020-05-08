4bd3060235fb20e3b902ea74ef87acb99a609ab8
HeimirDavid <heimir.97@gmail.com>
2020-05-08 20:05:39 +0200

    user orders page only accessible when a user is lgged in and has made an order. The page has basic styling now and a button in the navbar has been added when a user is logged in

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import permission_required, login_required
 from checkout.models import Order
 
 # Create your views here.
@@ -38,13 +40,17 @@ def view_single_order(request, pk):
     return render(request, 'singleorder.html', context)
 
 
+@login_required
 def user_orders(request):
     current_user = request.user
+    
     user_orders = Order.objects.filter(user=current_user)
+    order_list = list(user_orders.values())
+    if not order_list:
+        messages.error(request, "You don't have any orders yet. Please keep shopping")
+        return redirect(reverse('index'))
 
-    print(user_orders)
     context = {
         'user_orders': user_orders,
-
     }
     return render(request, "userorders.html", context)
