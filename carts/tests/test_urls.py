27d95cd7bf0ef13da334acdb5c0f6762333ac9b1
HeimirDavid <heimir.97@gmail.com>
2020-05-09 13:07:49 +0200

    Unit test for urls in carts app added

---
+++ carts/tests/test_urls.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from carts.views import view_cart, add_to_cart, remove_from_cart, clear_cart
+
+
+
+class TestUrls(TestCase):
+    """
+    Simple test to see if the ulrs resolves,
+    source: https://www.youtube.com/watch?v=0MrgsYswT1c&list=PLbpAWbHbi5rMF2j5n6imm0enrSD9eQUaM&index=2
+    """
+    def test_view_cart_url_resolves(self):
+        url = reverse('view_cart')
+        self.assertEquals(resolve(url).func, view_cart)
+
+
+    def test_add_to_cart_url_resolves(self):
+        url = reverse('add_to_cart', args=['1']) #expects some number
+        self.assertEquals(resolve(url).func, add_to_cart)
+
+
+    def test_remove_from_cart_url_resolves(self):
+        url = reverse('remove_from_cart', args=['1']) #expects some number
+        self.assertEquals(resolve(url).func, remove_from_cart)
+
+
+    def test_clear_cart_url_resolves(self):
+        url = reverse('clear_cart') #expects some number
+        self.assertEquals(resolve(url).func, clear_cart)
--- newsposts/views.py
+++ newsposts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import NewsPost
-from .forms import NewsPostForm
 
 
 def get_newsposts(request):
@@ -27,19 +26,3 @@ def news_detail(request, pk):
     post.save()
     return render(request, 'articlepost.html', {'post': post})
 
-
-"""
-def create_or_edit_newspost(request, pk=None):
-    
-    A View that allows us to create or edit a post,
-    varying if the NewPost ID is null or not
-    
-    post = get_object_or_404(NewsPost, pk=pk) if pk else None
-    if request.method == "POST":
-        form = NewsPostForm(request.POST, request.FILES, instance=post)
-        if form.is_valid:
-            post = form.save()
-            return redirect(news_detail, post.pk)
-    else:
-        form = NewsPostForm(instance=post)
-    return render(request, 'postform.html', {'form': form})"""
