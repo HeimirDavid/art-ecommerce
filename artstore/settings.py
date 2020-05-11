5fedf4320f6b9a689b046607af83acc5c8e654cd
HeimirDavid <heimir.97@gmail.com>
2020-05-11 20:57:44 +0200

    switched back, realised i forgot to push the code so allowed hosts wasn't in the build

--- artstore/settings.py
+++ artstore/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://lowgren-art.herokuapp.com/']
+ALLOWED_HOSTS = ['lowgren-art.herokuapp.com']
 
 
 # Application definition
