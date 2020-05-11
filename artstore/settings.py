ac23b8290b69c44834f007923b26561d4231ccbd
HeimirDavid <heimir.97@gmail.com>
2020-05-11 19:47:16 +0200

    Static files hosted using aws s3 bucket

--- artstore/settings.py
+++ artstore/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'accounts',
     'checkout',
     'orders',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -135,6 +136,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 #MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
+
+AWS_STORAGE_BUCKET_NAME = 'art-store-ms4'
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
