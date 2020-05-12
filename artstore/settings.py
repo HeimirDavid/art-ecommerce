9c41a7d89fbfe9fec56a7b8c37f56c4e245f8194
HeimirDavid <heimir.97@gmail.com>
2020-05-12 08:48:40 +0200

    Since deployment one mayor issue with overflow on mobile view appeared. The issue is due to bootstrap rows missing container and overflow-x on body not working. This issue has been fixed on index.html and base.html

--- artstore/settings.py
+++ artstore/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lowgren-art.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'lowgren-art.herokuapp.com']
 
 
 # Application definition
